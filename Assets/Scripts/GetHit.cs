adfc2da9558ba1dfa3ee0b21087751fbf6bc70f6
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2023-10-04 16:44:32 +0100

    Initial commit

---
+++ Assets/Scripts/GetHit.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetHit : MonoBehaviour
+{
+    [Tooltip("Determines when the player is taking damage.")]
+    public bool hurt = false;
+    
+    [Tooltip("How much health is removed from the player when Hit")]
+    [SerializeField] private int hitDamage = 0;
+    //The current health level
+    //private int health;
+    private bool slipping = false;
+    private PlayerMovement playerMovementScript;
+    private Rigidbody rb;
+    private Transform enemy;
+
+    private void Start()
+    {
+        playerMovementScript = GetComponent<PlayerMovement>();
+        //stompEnemiesScript = GetComponent<StompEnemies>();
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        // stops the player from running up the slopes and skipping platforms
+        if (slipping == true)
+        {
+            transform.Translate(Vector3.back * 20 * Time.deltaTime, Space.World);
+            playerMovementScript.playerStats.canMove = false;
+        }
+    }
+    private void OnCollisionStay(Collision other)
+    {
+        if (hurt == false)
+        {
+            if (other.gameObject.tag == "Enemy" && playerMovementScript.playerStats.canHurt)
+            {
+                enemy = other.gameObject.transform;
+                rb.AddForce(enemy.forward * 1000);
+                rb.AddForce(transform.up * 500);
+                TakeDamage();
+            }
+            if (other.gameObject.tag == "Trap")
+            {
+                rb.AddForce(transform.forward * -1000);
+                rb.AddForce(transform.up * 500);
+                TakeDamage();
+            }
+        }
+        if (other.gameObject.layer == 9)
+        {
+            slipping = true;
+        }
+        if (other.gameObject.layer != 9)
+        {
+            if (slipping == true)
+            {
+                slipping = false;
+                playerMovementScript.playerStats.canMove = true;
+            }
+        }
+    }
+    private void TakeDamage()
+    {
+        hurt = true;
+        playerMovementScript.playerStats.canMove = false;
+        playerMovementScript.soundManager.PlayHitSound();
+        //add in the reduction of health when hit
+        HealthManager.TakeHealthHit(hitDamage);
+        //HealthManager.TakeHealthHit();
+        StartCoroutine("Recover");
+    }
+    private IEnumerator Recover()
+    {
+        yield return new WaitForSeconds(0.75f);
+        hurt = false;
+        playerMovementScript.playerStats.canMove = true;
+    }
+}
---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    //The current health level
+    private static float health;
+    private static float maxHealth;
+    private static int coinCount = 0;
+    //how many coins make up a single health point
+    private static int coinPoint = 10;
+
+    public static void TakeHealthHit(float hitDamage)
+    {
+        health -= hitDamage;
+        if(health <= 0){
+            Debug.Log("WE HAVE NO HEALTH REMAINING");
+            health = 0;
+        }else{
+            PrintHealth();
+        }
+    }
+
+    private static void PrintHealth()
+    {
+        Debug.Log($"Health: {health}");
+    }
+    public static void ResetHealth(float startingHealth)
+    {
+        health = startingHealth;
+        maxHealth = startingHealth;
+    }
+
+    public static void AddCoinToHealth()
+    {
+        coinCount += 1;
+        Debug.Log($"Just {coinPoint-coinCount} more coins and you get a health bonus point");
+        if(coinCount >= coinPoint){
+            coinCount = 0;
+            health = (health < maxHealth) ? health + 1 : health;
+            PrintHealth();
+        }
+    }
+}
